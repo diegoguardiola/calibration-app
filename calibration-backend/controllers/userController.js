1c5eb158c6df4865d799c982b82ec1a31b8c2244
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-10-25 00:12:49 -0400

    able to combine all the data

--- calibration-backend/controllers/userController.js
+++ calibration-backend/controllers/userController.js
@@ -129,8 +129,19 @@ const getEquipmentByUserId = async (req, res) => {
 };
 
 const getInfoByUserId = async (req, res) => {
+  const userId = req.params.id;
 
-};
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
 
 
 
