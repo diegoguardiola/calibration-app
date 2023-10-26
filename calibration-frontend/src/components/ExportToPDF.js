e7176f05fd534239ff740cee5b95d64fa0ccb884
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-10-26 01:32:56 -0400

    report mostly done, styling needing to be changed

--- calibration-frontend/src/components/ExportToPDF.js
+++ calibration-frontend/src/components/ExportToPDF.js
@@ -28,14 +28,14 @@ export const exportToPDF = (calibration) => {
         bodyStyles: {
             fontSize: 8, 
             cellPadding: 0.2,
-            lineColor: [0, 0, 225],
+            lineColor: [0, 0, 153],
         },
         didDrawCell: (data) => {
             // If this is a head section cell and it is the last column
             if (data.section === 'head' ) {
                 // The y position just below the cell
                 let y = data.cell.y + data.cell.height;
-                doc.setDrawColor(0, 0, 255);
+                doc.setDrawColor(0, 0, 153);
                 // Draw a line from the left edge of the cell to the right
                 doc.line(data.cell.x, y, data.cell.x + data.cell.width, y);
             }
@@ -72,7 +72,7 @@ export const exportToPDF = (calibration) => {
             if (data.section === 'head' ) {
                 // The y position just below the cell
                 let y = data.cell.y + data.cell.height;
-                doc.setDrawColor(0, 0, 255);
+                doc.setDrawColor(0, 0, 153);
                 // Draw a line from the left edge of the cell to the right
                 doc.line(data.cell.x, y, data.cell.x + data.cell.width, y);
             }
@@ -96,7 +96,7 @@ export const exportToPDF = (calibration) => {
             if (data.section === 'head' ) {
                 // The y position just below the cell
                 let y = data.cell.y + data.cell.height;
-                doc.setDrawColor(0, 0, 255);
+                doc.setDrawColor(0, 0, 153);
                 // Draw a line from the left edge of the cell to the right
                 doc.line(data.cell.x, y, data.cell.x + data.cell.width, y);
             }
@@ -176,7 +176,7 @@ export const exportToPDF = (calibration) => {
         if (data.section === 'head' ) {
             // The y position just below the cell
             let y = data.cell.y + data.cell.height - 3;
-            doc.setDrawColor(0, 0, 255);
+            doc.setDrawColor(0, 0, 153);
             // Draw a line from the left edge of the cell to the right
             doc.line(data.cell.x, y, data.cell.x + data.cell.width, y);
         }
@@ -221,141 +221,26 @@ export const exportToPDF = (calibration) => {
         },
         tableWidth: 180,
     });
-
-    //pagebreak
-    doc.addPage();
-
-    // Logo
-    doc.addImage(logo, 'PNG', 75, 5, 50, 20);
-    // Calibration Table
-    const tableData = calibration.tests.testPoints.map((point) => ([
-        calibration.tests[0].testPoints.nominal, // Convert to a number if needed
-        calibration.tests[0].testPoints.asFound, // Convert to a number if needed
-        calibration.tests[0].testPoints.asLeft,   // Convert to a number if needed
-        calibration.tests[0].testPoints.result,
-        calibration.tests[0].testPoints.min,         // Convert to a number if needed
-        calibration.tests[0].testPoints.max
-    ]))
-    const tableStyles = {
-        borderColor: [0, 0, 0], // Black border color
-        fillColor: [255, 255, 255],   // Black fill color
-        textColor: [0, 0, 0], // White text color
-    };
-    // Configure cell styles
-    const cellStyles = {
-        lineWidth: 0.3, // Line width for cell borders
-        lineColor: [0, 0, 0], // Black cell border color
-    };
-
+    //Footer
     doc.autoTable({
-        startY: 35,
-        head: [['Function/Range', 'Nominal Value', 'As Found', 'As Left', 'Result', 'Min', 'Max', 'Units']],
-        body: tableData,
-        headStyles: {
-            fontSize: 9,
-            cellPadding: 0.3,
-            fillColor: [255, 255, 255], 
-            textColor: [0, 0, 0]
-        },
+        startY: 268,
+        head: [[{ content: "", colSpan: 2 }]],
+        body: [
+           ['2689 Citrus Rd Suite D, Rancho Cordova, CA 95742']
+        ],
         bodyStyles: {
             fontSize: 8, 
             cellPadding: 0.2,
-            lineColor: [0, 0, 225],
-        },
-        styles: tableStyles,
-        columnStyles: {
-            0: cellStyles,
-            1: cellStyles,
-            2: cellStyles,
-        },
-        theme: 'plain',
-        tableWidth: 150,
-    });
-
-
-
-
-        // Calibration Results
-        doc.autoTable({
-            startY: 470,
-            head: [['Setpoint', 'As Found', 'As Left']],
-            body: [
-                
-                //[calibration.setpoint1, calibration.asFound1, calibration.asLeft1],
-                //[calibration.setpoint2, calibration.asFound2, calibration.asLeft2],
-                //[calibration.setpoint3, calibration.asFound3, calibration.asLeft3],
-            ],
-            headStyles: {
-                fontSize: 9,
-                cellPadding: 0.3,
-                fillColor: [255, 255, 255], 
-                textColor: [0, 0, 0],
-                halign: 'center',
-            },
-            bodyStyles: {
-                fontSize: 8, 
-                cellPadding: 0.2,
-                halign: 'center',
-            },
-            
-            tableWidth: 470,
-        });
-
-    //Pass/Fail Statements
-    const textWidth = 365; // Desired width for text wrapping
-    const text1 = 'Statements of Pass or Fail Conformance: The uncertainty of the measurement has been considered when determining compliance with specification. All measurements and test results guard banded to ensure the probability of false-accepts does not exceed {guard percent} in compliance with {standard}';
-    const text2 = 'Field Environmental Conditions: Temperature: between 17 and 23 degrees Celsius; rate of change not to exceed 1.0 C per hour. Relative humidity: from 30 to 70%. These conditions satisfy NIST requirements.'
-    const text3 = 'IMPORTANT: The "Calibration valid through" date shown above has been specified to us by this client. Falco Automation, LLC takes no position regarding the suitability or appropriateness of this interval.'
-    const text4 = 'Calibration Report Status:'
-    const text5 = 'PASS- Term used when compliance statement is given, and the measurement result is within accepted criteria.'
-    const text6 = 'FAIL- Term used when compliance statement is given, and the measurement result falls out of accepted criteria. '
-    const text7 = 'ADJUSTED- When adjustments are made to an instrument which changes the value of measurement from what was measured as found to new value. as left'
-
-    const splitText1 = doc.splitTextToSize(text1, textWidth);
-    const splitText2 = doc.splitTextToSize(text2, textWidth);
-    const splitText3 = doc.splitTextToSize(text3, textWidth);
-    const splitText4 = doc.splitTextToSize(text4, textWidth);
-    const splitText5 = doc.splitTextToSize(text5, textWidth);
-    const splitText6 = doc.splitTextToSize(text6, textWidth);
-    const splitText7 = doc.splitTextToSize(text7, textWidth);
-    doc.setFontSize(8);
-
-    doc.text(splitText1, 10, 119);
-    doc.text(splitText2, 10, 134);
-    doc.text(splitText3, 10, 205);
-    doc.text(splitText4, 10, 220);
-    doc.text(splitText5, 10, 224);
-    doc.text(splitText6, 10, 228);
-    doc.text(splitText7, 10, 232);
-
-    // Comments
-    doc.autoTable({
-        startY: 140,
-        head: [["Comments"]],
-        body: [
-            //[calibration.comments],
-        ].map(item => [item]), // Wrapping each item in another array
-        headStyles: {
-            fontSize: 9,
-            cellPadding: 0.3,
-            halign: 'center',
-            fillColor: [255, 255, 255], 
-            textColor: [0, 0, 0] 
-        },
-        bodyStyles: {
-            fontSize: 8,
-            cellPadding: 0.2,
-            lineWidth: 0.5, // specify the line width for the table body
-            lineColor: [0, 0, 225],
-            minCellHeight: 30,
+            halign: 'left'
         },
         theme: 'plain',
         didDrawCell: (data) => {
             // If this is a head section cell and it is the last column
             if (data.section === 'head' ) {
                 // The y position just below the cell
-                let y = data.cell.y + data.cell.height;
-                doc.setDrawColor(0, 0, 255);
+                let y = data.cell.y + data.cell.height - 3;
+                doc.setDrawColor(0, 0, 153);
+                doc.setLineWidth(0.78); // Set the line thickness to 1 (or any other value you prefer)
                 // Draw a line from the left edge of the cell to the right
                 doc.line(data.cell.x, y, data.cell.x + data.cell.width, y);
             }
@@ -363,38 +248,85 @@ export const exportToPDF = (calibration) => {
         tableWidth: 180,
     });
 
-    // Convert the date format
-    //const dateParts = calibration.dateOfCalibration.split("T")[0].split("-");
-    //const formattedDateofCalibration = `${dateParts[0]}-${dateParts[1]}-${dateParts[2]}`;
-    //const dateParts2 = calibration.calibrationDueDate.split("T")[0].split("-");
-    //const formattedCalibrationDueDate = `${dateParts2[0]}-${dateParts2[1]}-${dateParts2[2]}`;
-    //const dateParts3 = calibration.createdAt.split("T")[0].split("-");
-    //const formattedCalibrationCreatedAt = `${dateParts3[0]}-${dateParts3[1]}-${dateParts3[2]}`;
-     // Sign Off
-     doc.autoTable({
-        startY: 175,
-        head: [[{ content: "", colSpan: 6 }]],
-        body: [
-            //['Calibration Technician', calibration.calibrationTech, 'Technician Signature', '___________________', 'Date of Calibration', formattedDateofCalibration],
-            //['', '', '', '', 'Calibration Due Date', formattedCalibrationDueDate],
-            //['Reviewed By', '', 'Reviewer Signature', '___________________', 'Date Cert Issued',   formattedCalibrationCreatedAt],
-        ],
+    //pagebreak
+    doc.addPage();
+
+    // Logo
+    doc.addImage(logo, 'PNG', 75, 5, 50, 20);
+
+    // Calibration Table
+    const tableData = [];
+    calibration.results.tests.forEach(test => {
+      // Insert a new row for the test method
+      tableData.push([{ content: test.method, colSpan: 8, styles: { 
+        fontStyle: 'bold', 
+        halign: 'center',
+        fillColor: [179, 175, 174], 
+    } }]);
+      // Map over the testPoints and add them to the tableData
+      const testPointsData = test.testPoints.map(point => [
+        test.type, // Assuming you want to include the test type as "Function/Range"
+        point.nominal,
+        point.asFound,
+        point.asLeft,
+        point.result,
+        point.min,
+        point.max,
+        test.unit // Assuming you want to include the test unit
+      ]);
+      tableData.push(...testPointsData);
+    });
+    const tableStyles = {
+        borderColor: [0, 0, 0], 
+        fillColor: [255, 255, 255],   
+        textColor: [0, 0, 0], 
+    };
+    const cellStyles = {
+        lineWidth: 0.3, // Line width for cell borders
+        lineColor: [0, 0, 0], // Black cell border color
+        halign: 'center'
+    };
+    doc.autoTable({
+        startY: 35,
+        head: [['Function/Range', 'Nominal Value', 'As Found', 'As Left', 'Result', 'Min', 'Max', 'Units']],
+        body: tableData,
         headStyles: {
-            fontSize: 10,
+            fontSize: 9,
             cellPadding: 0.3,
-            fillColor: [255, 255, 255], 
-            textColor: [0, 0, 0],
-            halign: 'center',
+            textColor: [255, 255, 255],
+            borderColor: [0, 0, 0], 
+            fillColor: [4, 31, 132], 
+            lineColor: [0, 0, 0],
+            halign: 'center'
         },
         bodyStyles: {
             fontSize: 8, 
             cellPadding: 0.2,
-            halign: 'center',
+            lineColor: [0, 0, 153],
         },
+        styles: tableStyles,
+        columnStyles: {
+            0: cellStyles,
+            1: cellStyles,
+            2: cellStyles,
+            3: cellStyles,
+            4: cellStyles,
+            5: cellStyles,
+            6: cellStyles,
+            7: cellStyles,
+        },
+        didDrawCell: (data) => {
+            if (data.row.section === 'head') {
+              doc.setDrawColor(0); // Set border color to black
+              doc.setLineWidth(0.3); // Set border width
+              doc.rect(data.cell.x, data.cell.y, data.cell.width, data.cell.height);
+            }
+          },
         theme: 'plain',
-        tableWidth: 170,
+        tableWidth: 180,
     });
 
-
+    //Footer
+    
     doc.save(`Calibration_${calibration.calibration_id}.pdf`);
 };
